a4e9a089530c89272ebdc854991d810511b2a954
Christian Morales <mmandamiento3@gmail.com>
2021-09-10 11:57:39 -0500

    Creación del Proyecto
    Se creo el proyecto en VS 2015_ ASP.NET MVC

---
+++ Proyecto Tesis/Proyecto Tesis/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_Tesis.MvcApplication" Language="C#" %>
---
+++ Proyecto Tesis/Proyecto Tesis/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_Tesis.Startup))]
+namespace Proyecto_Tesis
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
